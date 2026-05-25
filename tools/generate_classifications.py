ab1cfca1fd91dd1f24244ddbd48ab57a2ef9c5c6
carlopav <carlopav@gmail.com>
2026-05-25 21:39:04 +0200

    license

--- tools/generate_classifications.py
+++ tools/generate_classifications.py
@@ -1,3 +1,23 @@
+# Bonsai - OpenBIM 5D Blender Add-on based on Bonsai
+# Copyright (C) 2026 Carlo Pavan <carlopav@gmail.com>
+#
+# This file is part of Bonsai5D+.
+#
+# Bonsai5D+ is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Bonsai5D+ is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Bonsai5D+.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This file was modified with the assistance of an AI coding tool.
+
 """
 Generate IFC4X3 classification library files for SOA, DM17 Z-1 and TOL.
 
